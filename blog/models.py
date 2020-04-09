82cd94035657a7e9ec6b2f59a182112b3d2a223b
asibubernard <asibubernard@gmail.com>
2020-04-09 12:57:38 +0000

    added CREATE,EDIT, and DELETE functions to the blog

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse # new
 
 # Create your models/database/table here.
 
@@ -9,3 +10,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self): # new
+        return reverse('post_detail', args=[str(self.id)])
